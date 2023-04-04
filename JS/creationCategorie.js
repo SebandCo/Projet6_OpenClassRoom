08efffc52d5f3686b75e5461e6fbe9d1c8195438
SebandCo <113468072+SebandCo@users.noreply.github.com>
2023-04-04 11:48:41 +0200

    Correction bug si la catégorie à un nombre limite de film

--- JS/creationCategorie.js
+++ JS/creationCategorie.js
@@ -35,20 +35,28 @@ export function creationTitreCategorie (genres){
 export async function creationFilmCategorie(api, nbrFilmAffiche, genres){   
     // appel de l'API
     for (let i=0; i<genres.length;i++){
-        // Page 1 par rapport au genre choisi et trié du meilleurs imdb au moins
-        let reponseServeurP1 = await fetch(api+'/titles/?page=1&genre='+genres[i]+'&sort_by=-imdb_score');
-        // Mise en Json de la réponse API
-        reponseServeurP1 = await reponseServeurP1.json();
-        // Récupération de la categorie results de la requete Json
-        reponseServeurP1 = reponseServeurP1.results
-        // Idem sur Page 2
-        let reponseServeurP2 = await fetch(api+'/titles/?page=2&genre='+genres[i]+'&sort_by=-imdb_score');
-        reponseServeurP2 = await reponseServeurP2.json();
-        reponseServeurP2 = reponseServeurP2.results
-        // concatenation des deux resultats JSON
-        const reponseServeur = reponseServeurP1.concat(reponseServeurP2);
+        let reponseServeur=""
+        //Récupération de la page 1
+            // Page 1 par rapport au genre choisi et trié du meilleurs imdb au moins
+            let reponseServeurP1 = await fetch(api+'/titles/?page=1&genre='+genres[i]+'&sort_by=-imdb_score');
+            // Mise en Json de la réponse API
+            reponseServeurP1 = await reponseServeurP1.json();
+            // Récupération de la categorie results de la requete Json
+            reponseServeurP1 = reponseServeurP1.results
+        // Récupération de la Page 2 à condition qu'elle existe
+            let reponseServeurP2 = await fetch(api+'/titles/?page=2&genre='+genres[i]+'&sort_by=-imdb_score');
+            if (reponseServeurP2.status!="404"){
+                reponseServeurP2 = await reponseServeurP2.json();
+                reponseServeurP2 = reponseServeurP2.results
+                // concatenation des deux resultats JSON
+                reponseServeur = reponseServeurP1.concat(reponseServeurP2);
+            }
+            else{
+                reponseServeur = reponseServeurP1
+            }
+
         // Affichage des nbrFilmAffiche meilleurs films
-        for (let j=0; j<nbrFilmAffiche; j++){
+        for (let j=0; j<nbrFilmAffiche && j<reponseServeur.length; j++){
             await genererFilm(reponseServeur[j],("#categorie"+(i+1)))
         }
     }
